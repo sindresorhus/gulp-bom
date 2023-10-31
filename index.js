40a561bfcf801f670ba6f48a48742a77c3ed39c8
Sindre Sorhus <sindresorhus@gmail.com>
2023-11-01 02:00:59 +0700

    Require Node.js 18

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
 import {Buffer} from 'node:buffer';
-import transformStream from 'easy-transform-stream';
-import PluginError from 'plugin-error';
+import {gulpPlugin} from 'gulp-plugin-extras';
 
 const BOM = Buffer.from('\uFEFF');
 
 export default function gulpBom() {
-	return transformStream({objectMode: true}, async file => {
-		if (file.isNull()) {
-			return file;
-		}
-
-		if (file.isStream()) {
-			throw new PluginError('gulp-bom', 'Streaming not supported');
-		}
-
+	return gulpPlugin('gulp-bom', file => {
 		file.contents = Buffer.concat([BOM, file.contents]);
-
 		return file;
 	});
 }
